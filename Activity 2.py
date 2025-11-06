f2ac77c9a99ffda40f4b3bd3a322bd53edaf6d22
KKwins04 <akavinkarthik@gmail.com>
2025-11-06 16:31:06 +0800

    Activity 2 commit

---
+++ Activity 2.py
@@ -0,0 +1,14 @@
+#Activity 2
+#Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#User input
+name = input("Enter your name: ")
+
+print("\n Hello", name, "\n Welocome to codingal")
