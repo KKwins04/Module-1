9f29640498bddfbadea2f8753db19fd37ff29fd5
KKwins04 <akavinkarthik@gmail.com>
2025-11-06 16:22:42 +0800

    initial commit

---
+++ Activity 1.py
@@ -0,0 +1,15 @@
+#Activity 1
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+print(4)
+
+#Next line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("Hello" ,4)
+
+#End argument for Print statement
+print("welcome too ", end= "$") 
