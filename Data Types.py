dd13babb52d732570658f5481ae586c43f7054a6
KKwins04 <akavinkarthik@gmail.com>
2025-11-10 15:58:14 +0800

    Data Types commit

---
+++ Data Types.py
@@ -0,0 +1,12 @@
+#Lets check the datatype of different variables
+a = 5
+print("type of a: ", type(a))
+
+b = 2.5
+print("type of b: ", type(b))
+
+c = "codingal"
+print("type of c: ", type(c))
+
+d = True
+print("type of d: ", type(d))
