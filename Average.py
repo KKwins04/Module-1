7b61b89b2120abb44f94c7363def6031560c3705
KKwins04 <akavinkarthik@gmail.com>
2025-11-11 16:12:26 +0800

    Average commit

---
+++ Average.py
@@ -0,0 +1,14 @@
+#Storing Values
+tree1 = 98
+tree2 = 90
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+#Finding the total of trees
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("THe sum of the trees is", sum)
+
+#Finding the average of trees
+average = sum/5
+print("The average of the trees is", average)
