7207162b2f73d0b03ce35a302c50fa403bc23d1a
KKwins04 <akavinkarthik@gmail.com>
2025-11-11 15:08:41 +0800

    Homework 1 redo commit

---
+++ Homework 1 redo.py
@@ -0,0 +1,14 @@
+# Store 5 birthdays as variables
+Alice_birthday = "January 5, 2012"
+Ben_birthday = "March 12, 2011"
+Charlie_birthday = "July 24, 2013"
+Dexter_birthday = "October 18, 2011"
+Edgar_birthday = "December 30, 2012"
+
+# Print them for the user to see
+print("Here are the stored birthdays:")
+print("Alice.", Alice_birthday)
+print("Ben.", Ben_birthday)
+print("Charlie.", Charlie_birthday)
+print("Dexter.", Dexter_birthday)
+print("Edgar.", Edgar_birthday)
