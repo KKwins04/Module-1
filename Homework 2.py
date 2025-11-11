7cb9a7012fcc98878e6248c354223ded4642a3ab
KKwins04 <akavinkarthik@gmail.com>
2025-11-11 15:14:30 +0800

    Homework 2 commit

---
+++ Homework 2.py
@@ -0,0 +1,9 @@
+# Raj enters his message in lowercase
+message = input("Enter your message: ")
+
+# Convert the message to uppercase
+uppercase_message = message.upper()
+
+# Print the uppercase version
+print("Here is your message in uppercase:")
+print(uppercase_message)
