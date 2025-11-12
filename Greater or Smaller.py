fdfea1f75c212a382f3513c4a04cf4e622700e21
KKwins04 <akavinkarthik@gmail.com>
2025-11-12 17:05:56 +0800

    Greater or Smaller commit

---
+++ Greater or Smaller.py
@@ -0,0 +1,11 @@
+n =int(input("Enter the number: "))
+
+if(n < 15):
+    print("The number is smaller than 15")
+    print("in if block")
+
+else: 
+    print("The number is greater than 15")
+    print("in else block")
+
+print("outside if and else")
