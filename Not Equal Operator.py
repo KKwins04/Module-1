ace5dce0042eca4dff45aa3c5c48e499431a9105
KKwins04 <akavinkarthik@gmail.com>
2025-11-17 16:07:36 +0800

    Not Equal Operator commit

---
+++ Not Equal Operator.py
@@ -0,0 +1,18 @@
+a = 67
+b = "python"
+c = "python"
+
+print(a !=b)
+print(b !=c)
+
+a = "python"
+b = "coding"
+
+if a != b :
+    print(a, "and", b, "are different")
+
+a = 4
+b = 5
+
+if(a == 1) != (b == 5):
+    print("Hello")
