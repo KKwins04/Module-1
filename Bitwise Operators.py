952395ea3dd7a83ac8fa85bd4668a01393d00544
KKwins04 <akavinkarthik@gmail.com>
2025-11-18 16:25:30 +0800

    Bitwise Operators commit

---
+++ Bitwise Operators.py
@@ -0,0 +1,15 @@
+a = 7 #0101
+b = -2 #1010
+
+# print bitwise right shift operator
+# cuts number into half (rounding down)
+print("a >> 1 =", a >> 1)
+print("b >> 1 =", b >> 1)
+
+a = 6
+b = -5
+
+#print bitwise left shift operator
+# doubles the number
+print("a << 1 =", a << 1)
+print("b << 1 =", b << 1)
