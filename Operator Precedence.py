eb0681f99afac39b208c50f417484dc0ffcc6587
KKwins04 <akavinkarthik@gmail.com>
2025-11-20 16:20:35 +0800

    Operator Precedence commit

---
+++ Operator Precedence.py
@@ -0,0 +1,16 @@
+v = 3
+w = 6
+x = 4
+y = 20
+z = 0
+z = (v + y) * x / w
+print("Value of (v + w) * x / y is: ", z)
+
+
+name = "Alex"
+age = 0
+
+if name == "Alex" or name == "John" and age >= 2:
+    print("Hello! Welcome.")
+else:
+    print("Good Bye!!")
